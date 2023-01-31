e21d99ed94a1c59f73f3f46703812508aee9eeea
Silva-6 <silva6media@gmail.com>
2023-01-31 10:58:13 +0100

    success

--- 0x01-convert_booklist_API_project/BookList/BookListDRF/views.py
+++ 0x01-convert_booklist_API_project/BookList/BookListDRF/views.py
@@ -7,4 +7,8 @@ from rest_framework import generics
 class BookView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+class SingleBookView(generics.RetrieveUpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
     
