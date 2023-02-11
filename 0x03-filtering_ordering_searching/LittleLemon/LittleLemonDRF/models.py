45aff384b53290aa916e0b6883a3374f64dd1ad2
Silva-6 <silva6media@gmail.com>
2023-02-11 22:23:55 +0100

    updated

--- 0x03-filtering_ordering_searching/LittleLemon/LittleLemonDRF/models.py
+++ 0x03-filtering_ordering_searching/LittleLemon/LittleLemonDRF/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.model):
+    slug = models.SlugField()
+    title = models.CharField(max_length=255)
+
+
+class MenuItem(models.Model):
+    title = models.SlugField()
+    price = models.CharField(max_length=255)
+    inventory = models.SmallIntegerField(max_digit=6, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, default=1)
